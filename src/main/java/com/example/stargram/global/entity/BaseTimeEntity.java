663b8a2ccd4b18c16a29e0b707ad976eb50cd72b
heesung33 <chaeheesung33@gmail.com>
2022-09-06 20:00:20 +0900

    ➕ ::  생성시간 기능 추가

---
+++ src/main/java/com/example/stargram/global/entity/BaseTimeEntity.java
@@ -0,0 +1,16 @@
+package com.example.stargram.global.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.ZonedDateTime;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseTimeEntity {
+
+    @Column(nullable = false)
+    private final ZonedDateTime create_at = ZonedDateTime.now();
+
+}
