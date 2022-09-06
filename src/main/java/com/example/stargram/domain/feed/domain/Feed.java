331caca8eb582960052925dc809eb0d2f89a700a
heesung33 <chaeheesung33@gmail.com>
2022-09-06 20:00:55 +0900

    ➕ ::  feed entity 생성

--- src/main/java/com/example/stargram/domain/feed/domain/Feed.java
+++ src/main/java/com/example/stargram/domain/feed/domain/Feed.java
@@ -1,2 +1,36 @@
-package com.example.stargram.domain.feed.domain;public class Feed {
+package com.example.stargram.domain.feed.domain;
+
+
+import com.example.stargram.domain.user.domain.User;
+import com.example.stargram.global.entity.BaseTimeEntity;
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Feed extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "BINARY(16)")
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, columnDefinition = "BINARY(16)")
+    private User user;
+
+    @NotNull
+    @Column(length = 30)
+    private String title;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    @Column(length = 10)
+    private String field;
 }
