a944bb18297c77fc7ad1821652c3885d49a44286
heesung33 <chaeheesung33@gmail.com>
2022-09-07 11:32:36 +0900

    🔄 :: JpaRepository에서 CrudRepository로 변경

--- src/main/java/com/example/stargram/domain/feed/domain/repository/FeedRepository.java
+++ src/main/java/com/example/stargram/domain/feed/domain/repository/FeedRepository.java
@@ -1,9 +1,9 @@
 package com.example.stargram.domain.feed.domain.repository;
 
 import com.example.stargram.domain.feed.domain.Feed;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
-public interface FeedRepository extends JpaRepository<Feed, UUID> {
+public interface FeedRepository extends CrudRepository<Feed, UUID> {
 }
