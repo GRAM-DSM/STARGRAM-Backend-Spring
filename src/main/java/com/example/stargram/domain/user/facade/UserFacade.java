5bca82559520f22f962055f838edf2868b4bfd25
heesung33 <chaeheesung33@gmail.com>
2022-09-07 09:23:56 +0900

    ➕ ::  UserFacade 생성

---
+++ src/main/java/com/example/stargram/domain/user/facade/UserFacade.java
@@ -0,0 +1,30 @@
+package com.example.stargram.domain.user.facade;
+
+import com.example.stargram.domain.user.domain.User;
+import com.example.stargram.domain.user.domain.repository.UserRepository;
+import com.example.stargram.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class UserFacade {
+
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getUserByAccountId(accountId);
+    }
+
+    public User getUserByAccountId(String id) {
+        return userRepository.findByAccountId(id)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+}
