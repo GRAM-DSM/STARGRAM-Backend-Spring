6904306272f1f1a6eae7f1f629cf2b9f72ef3291
heesung33 <chaeheesung33@gmail.com>
2022-09-07 09:24:49 +0900

    ➕ :: CreateFeedService 기능 생성

--- src/main/java/com/example/stargram/domain/feed/service/CreateFeedService.java
+++ src/main/java/com/example/stargram/domain/feed/service/CreateFeedService.java
@@ -1,4 +1,31 @@
 package com.example.stargram.domain.feed.service;
 
+import com.example.stargram.domain.feed.domain.Feed;
+import com.example.stargram.domain.feed.domain.repository.FeedRepository;
+import com.example.stargram.domain.feed.presentation.dto.request.CreateFeedRequest;
+import com.example.stargram.domain.user.domain.User;
+import com.example.stargram.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class CreateFeedService {
+
+    private final FeedRepository feedRepository;
+
+    private final UserFacade userFacade;
+
+    @Transactional
+    public void execute(CreateFeedRequest createFeedRequest) {
+        User user = userFacade.getCurrentUser();
+
+        feedRepository.save(Feed.builder()
+                        .title(createFeedRequest.getTitle())
+                        .user(user)
+                        .category(createFeedRequest.getCategory())
+                        .content(createFeedRequest.getContent())
+                .build());
+    }
 }
