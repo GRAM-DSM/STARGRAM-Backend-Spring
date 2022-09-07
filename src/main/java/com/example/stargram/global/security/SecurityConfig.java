644e357bc74232c77288bb071241d8d85012ba4f
heesung33 <chaeheesung33@gmail.com>
2022-09-07 20:55:31 +0900

    ➕ ::  bean 수정

--- src/main/java/com/example/stargram/global/security/SecurityConfig.java
+++ src/main/java/com/example/stargram/global/security/SecurityConfig.java
@@ -1,35 +1,33 @@
 package com.example.stargram.global.security;
 
 import lombok.RequiredArgsConstructor;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-
-
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception{
-        getHttp()
-                .csrf().disable()
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
                 .cors().and()
+                .csrf().disable()
                 .formLogin().disable()
 
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
                 .and()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll();
 
+                .anyRequest().authenticated()
 
+                .and()
+                .build();
     }
 }
