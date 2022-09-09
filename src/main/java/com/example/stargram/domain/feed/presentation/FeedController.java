18240bf358adf3cadb72a6b62401281ae496852d
heesung33 <chaeheesung33@gmail.com>
2022-09-09 19:58:07 +0900

    ➕ :: FeedController delete 기능 추가

--- src/main/java/com/example/stargram/domain/feed/presentation/FeedController.java
+++ src/main/java/com/example/stargram/domain/feed/presentation/FeedController.java
@@ -2,9 +2,12 @@ package com.example.stargram.domain.feed.presentation;
 
 import com.example.stargram.domain.feed.presentation.dto.request.CreateFeedRequest;
 import com.example.stargram.domain.feed.service.CreateFeedService;
+import com.example.stargram.domain.feed.service.DeleteFeedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RequestMapping("/feeds")
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class FeedController {
 
     private final CreateFeedService createFeedService;
+    private final DeleteFeedService deleteFeedService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
@@ -19,4 +23,9 @@ public class FeedController {
         createFeedService.execute(request);
     }
 
+    @DeleteMapping("/delete/{id}")
+    public void deleteFeed(@PathVariable UUID id) {
+        deleteFeedService.execute(id);
+    }
+    
 }
