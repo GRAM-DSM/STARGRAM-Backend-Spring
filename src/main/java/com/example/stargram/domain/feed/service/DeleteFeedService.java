c8c0ba7dbe9687ea36b6bcb62752e44f22f132e8
heesung33 <chaeheesung33@gmail.com>
2022-09-09 19:57:27 +0900

    ➕ ::  DeleteFeedService 기능 추가

--- src/main/java/com/example/stargram/domain/feed/service/DeleteFeedService.java
+++ src/main/java/com/example/stargram/domain/feed/service/DeleteFeedService.java
@@ -1,8 +1,22 @@
 package com.example.stargram.domain.feed.service;
 
 
+import com.example.stargram.domain.feed.domain.repository.FeedRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.UUID;
+
+
+@RequiredArgsConstructor
 @Service
 public class DeleteFeedService {
+
+    private final FeedRepository feedRepository;
+
+    @Transactional
+    public void execute(UUID id) {
+        feedRepository.deleteById(id);
+    }
 }
