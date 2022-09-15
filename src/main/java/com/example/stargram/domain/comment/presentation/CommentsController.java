1a6db63b5a551d1905f959e4faa6b2a3cef67b3e
heesung33 <chaeheesung33@gmail.com>
2022-09-15 10:28:08 +0900

    ➕ :: CommentsController 생성

---
+++ src/main/java/com/example/stargram/domain/comment/presentation/CommentsController.java
@@ -0,0 +1,22 @@
+package com.example.stargram.domain.comment.presentation;
+
+import com.example.stargram.domain.comment.presentation.dto.request.CreateCommentRequest;
+import com.example.stargram.domain.comment.service.CreateCommentsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/feeds/comments")
+@RequiredArgsConstructor
+@RestController
+public class CommentsController {
+
+    private final CreateCommentsService createCommentService;
+
+    @PostMapping
+    public void create(@RequestBody CreateCommentRequest createCommentRequest, @PathVariable UUID id) {
+        createCommentService.execute(createCommentRequest, id);
+
+    }
+}
