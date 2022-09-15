c11a2a768a88f50f5d62b6bf8c095a300dd8dd15
heesung33 <chaeheesung33@gmail.com>
2022-09-15 19:47:53 +0900

    ➕ :: Header 수정

--- src/main/java/com/example/stargram/domain/feed/service/DeleteFeedService.java
+++ src/main/java/com/example/stargram/domain/feed/service/DeleteFeedService.java
@@ -1,7 +1,12 @@
 package com.example.stargram.domain.feed.service;
 
 
+import com.example.stargram.domain.feed.domain.Feed;
 import com.example.stargram.domain.feed.domain.repository.FeedRepository;
+import com.example.stargram.domain.feed.exception.FeedNotFoundException;
+import com.example.stargram.domain.user.domain.User;
+import com.example.stargram.domain.user.facade.UserFacade;
+import com.example.stargram.global.exception.InvalidRoleException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,10 +18,20 @@ import java.util.UUID;
 @Service
 public class DeleteFeedService {
 
+    private final UserFacade userFacade;
     private final FeedRepository feedRepository;
 
     @Transactional
     public void execute(UUID id) {
-        feedRepository.deleteById(id);
+        User user = userFacade.getCurrentUser();
+
+        Feed feed = feedRepository.findById(id)
+                .orElseThrow(()-> FeedNotFoundException.EXCEPTION);
+
+        if(!feed.getUser().equals(user)){
+            throw InvalidRoleException.EXCEPTION;
+        }
+
+        feedRepository.delete(feed);
     }
 }
