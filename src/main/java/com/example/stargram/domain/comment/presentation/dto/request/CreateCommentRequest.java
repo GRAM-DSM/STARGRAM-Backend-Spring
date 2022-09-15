347662c37e736a746808e0ca3e6d0e8ae1f029fc
heesung33 <chaeheesung33@gmail.com>
2022-09-15 10:27:56 +0900

    ➕ :: Request feed 제거

--- src/main/java/com/example/stargram/domain/comment/presentation/dto/request/CreateCommentRequest.java
+++ src/main/java/com/example/stargram/domain/comment/presentation/dto/request/CreateCommentRequest.java
@@ -1,15 +1,12 @@
 package com.example.stargram.domain.comment.presentation.dto.request;
 
 
-import com.example.stargram.domain.feed.domain.Feed;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class CreateCommentRequest {
-
-    private Feed feed;
 
     private String content;
 }
