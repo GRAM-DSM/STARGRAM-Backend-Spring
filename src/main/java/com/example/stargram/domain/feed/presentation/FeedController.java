193226dd8138f1df08d155cd4d333fbc71b95fe8
heesung33 <chaeheesung33@gmail.com>
2022-09-15 20:07:44 +0900

    ➕ :: Feed update controller 추가

--- src/main/java/com/example/stargram/domain/feed/presentation/FeedController.java
+++ src/main/java/com/example/stargram/domain/feed/presentation/FeedController.java
@@ -1,8 +1,10 @@
 package com.example.stargram.domain.feed.presentation;
 
 import com.example.stargram.domain.feed.presentation.dto.request.CreateFeedRequest;
+import com.example.stargram.domain.feed.presentation.dto.request.UpdateFeedRequest;
 import com.example.stargram.domain.feed.service.CreateFeedService;
 import com.example.stargram.domain.feed.service.DeleteFeedService;
+import com.example.stargram.domain.feed.service.UpdateFeedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ public class FeedController {
 
     private final CreateFeedService createFeedService;
     private final DeleteFeedService deleteFeedService;
+    private final UpdateFeedService updateFeedService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
@@ -27,5 +30,10 @@ public class FeedController {
     public void deleteFeed(@PathVariable UUID id) {
         deleteFeedService.execute(id);
     }
-    
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/{id}")
+    public void updateFeed(@PathVariable UUID id, @RequestBody UpdateFeedRequest updateFeedRequest) {
+        updateFeedService.execute(id, updateFeedRequest);
+    }
 }
