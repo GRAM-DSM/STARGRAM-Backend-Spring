61cbcca4fbd10eea2d5300fd05e0d3bdb8e9d51b
heesung33 <chaeheesung33@gmail.com>
2022-09-19 22:16:48 +0900

    ➕ :: SnsException

--- src/main/java/com/example/stargram/global/error/exception/SnsException.java
+++ src/main/java/com/example/stargram/global/error/exception/SnsException.java
@@ -1,11 +1,12 @@
 package com.example.stargram.global.error.exception;
 
+import com.example.stargram.global.error.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class GlobalException extends RuntimeException{
+public class SnsException extends RuntimeException{
 
     private final ErrorCode errorCode;
 }
