9dcb802affdcf0dbf41d27cb1fac4bd4a237299d
heesung33 <chaeheesung33@gmail.com>
2022-09-19 22:19:38 +0900

    ➕ :: ExpiredAcessTokenException

---
+++ src/main/java/com/example/stargram/domain/auth/exception/ExpiredAccessTokenException.java
@@ -0,0 +1,13 @@
+package com.example.stargram.domain.auth.exception;
+
+import com.example.stargram.global.error.ErrorCode;
+import com.example.stargram.global.error.exception.SnsException;
+
+public class ExpiredAccessTokenException extends SnsException {
+    public static SnsException EXCEPTION =
+            new ExpiredAccessTokenException();
+
+    private ExpiredAccessTokenException() {
+        super(ErrorCode.EXPIRED_ACCESS_TOKEN);
+    }
+}
