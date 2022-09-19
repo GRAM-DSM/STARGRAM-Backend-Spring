19573cf493af6c5a4cbcc91d9d07be4ad9715756
heesung33 <chaeheesung33@gmail.com>
2022-09-19 22:19:12 +0900

    ➕ :: IncorrectTokenException

---
+++ src/main/java/com/example/stargram/domain/auth/exception/IncorrectTokenException.java
@@ -0,0 +1,13 @@
+package com.example.stargram.domain.auth.exception;
+
+import com.example.stargram.global.error.ErrorCode;
+import com.example.stargram.global.error.exception.SnsException;
+
+public class IncorrectTokenException extends SnsException {
+    public static SnsException EXCEPTION =
+            new IncorrectTokenException();
+
+    private IncorrectTokenException() {
+        super(ErrorCode.INCORRECT_TOKEN);
+    }
+}
