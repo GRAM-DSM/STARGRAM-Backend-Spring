7b1009aa267ea2e1170eb981f1f9a391a5975a5b
heesung33 <chaeheesung33@gmail.com>
2022-09-21 08:47:31 +0900

    ➕ :: SecurityConfig

--- src/main/java/com/example/stargram/global/security/config/SecurityConfig.java
+++ src/main/java/com/example/stargram/global/security/config/SecurityConfig.java
@@ -1,19 +1,26 @@
 package com.example.stargram.global.security.config;
 
+import com.example.stargram.global.security.jwt.JwtTokenProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -35,6 +42,9 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.POST,"/auth/signin").permitAll()
 
                 .and()
+                .apply(new FilterConfig(jwtTokenProvider, objectMapper))
+                .and()
+
                 .build();
     }
 }
