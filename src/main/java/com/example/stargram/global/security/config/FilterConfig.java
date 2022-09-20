84fa4be86a231dd2c58851317f1dd2430bcbfbf7
heesung33 <chaeheesung33@gmail.com>
2022-09-20 19:17:40 +0900

    ➕ :: FilterConfig

--- src/main/java/com/example/stargram/global/security/config/FilterConfig.java
+++ src/main/java/com/example/stargram/global/security/config/FilterConfig.java
@@ -1,8 +1,9 @@
 package com.example.stargram.global.security.config;
 
-import com.example.stargram.global.error.ExceptionHandlerFilter;
+import com.example.stargram.global.error.ExceptionFilter;
 import com.example.stargram.global.security.jwt.JwtTokenFilter;
 import com.example.stargram.global.security.jwt.JwtTokenProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,13 +14,14 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
 
     @Override
-    public void configure(HttpSecurity security) {
+    public void configure(HttpSecurity builder) {
         JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
-        ExceptionHandlerFilter exceptionHandlerFilter = new ExceptionHandlerFilter();
-
-        security.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
-        security.addFilterBefore(exceptionHandlerFilter, JwtTokenFilter.class);
+        ExceptionFilter exceptionFilter = new ExceptionFilter(objectMapper);
+        builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        builder.addFilterBefore(exceptionFilter, JwtTokenFilter.class);
     }
+
 }
