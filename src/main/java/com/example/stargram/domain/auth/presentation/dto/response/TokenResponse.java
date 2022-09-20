2f056096e446902efca58e4806a076f9b3da1391
heesung33 <chaeheesung33@gmail.com>
2022-09-20 19:16:20 +0900

    ➕ :: TokenResponse

--- src/main/java/com/example/stargram/domain/auth/presentation/dto/response/TokenResponse.java
+++ src/main/java/com/example/stargram/domain/auth/presentation/dto/response/TokenResponse.java
@@ -1,10 +1,12 @@
 package com.example.stargram.domain.auth.presentation.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class TokenResponse {
 
     private String accessToken;
