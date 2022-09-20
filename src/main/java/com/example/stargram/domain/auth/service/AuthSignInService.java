06bed99c8739b3573cc90c29e4c565a4db48b2f3
heesung33 <chaeheesung33@gmail.com>
2022-09-20 19:16:30 +0900

    ➕ :: AuthSignInService

--- src/main/java/com/example/stargram/domain/auth/service/AuthSignInService.java
+++ src/main/java/com/example/stargram/domain/auth/service/AuthSignInService.java
@@ -1,55 +1,43 @@
 package com.example.stargram.domain.auth.service;
 
-import com.example.stargram.domain.auth.domain.RefreshToken;
-import com.example.stargram.domain.auth.domain.repository.RefreshTokenRepository;
+
 import com.example.stargram.domain.auth.exception.InvalidPasswordException;
 import com.example.stargram.domain.auth.presentation.dto.request.SignInRequest;
 import com.example.stargram.domain.auth.presentation.dto.response.TokenResponse;
 import com.example.stargram.domain.user.domain.User;
 import com.example.stargram.domain.user.domain.repository.UserRepository;
 import com.example.stargram.domain.user.exception.UserNotFoundException;
+import com.example.stargram.global.annotation.ServiceWithTransactionalReadOnly;
 import com.example.stargram.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
-@Service
+@ServiceWithTransactionalReadOnly
 public class AuthSignInService {
 
-
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
-    private final RefreshTokenRepository refreshTokenRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    @Value("${jwt.exp.refresh}")
-    private Long REFRESH_TOKEN_VALID_TIME;
+    @Transactional
+    public TokenResponse execute(SignInRequest request) {
+        User user = userRepository.findByAccountId(request.getAccountId())
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
 
-    public TokenResponse execute(SignInRequest signInRequest) {
-        User user = userRepository.findByAccountId(signInRequest.getAccountId())
-                .orElseThrow(()-> UserNotFoundException.EXCEPTION);
 
-        if (!passwordEncoder.matches(signInRequest.getPassword(), user.getPassword())) {
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             throw InvalidPasswordException.EXCEPTION;
         }
 
-        return createToken(signInRequest.getAccountId());
-    }
+        String accessToken = jwtTokenProvider.generateAccessToken(user.getAccountId());
+        String refreshToken = jwtTokenProvider.generateRefreshToken(user.getAccountId());
 
-    public TokenResponse createToken(String name) {
-        String accessToken = jwtTokenProvider.createJwtAccessToken(name);
-        String refreshToken = jwtTokenProvider.createJwtRefreshToken(name);
-
-        refreshTokenRepository.save(
-                RefreshToken.builder()
-                        .userName(name)
-                        .refreshToken(refreshToken)
-                        .refreshExpiration(REFRESH_TOKEN_VALID_TIME)
-                        .build());
-
-        return new TokenResponse(accessToken, refreshToken);
+        return TokenResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
     }
 
 }
