0f6be8c1dfc011455c192975002797562ca069eb
heesung33 <chaeheesung33@gmail.com>
2022-09-21 14:58:58 +0900

    🔥 :: feedlike

--- src/main/java/com/example/stargram/domain/feedlike/domain/repository/FeedLikeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.stargram.domain.feedlike.domain.repository;
-
-import com.example.stargram.domain.feedlike.domain.FeedLike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.math.BigInteger;
-
-public interface FeedLikeRepository extends JpaRepository <FeedLike, BigInteger> {
-}
