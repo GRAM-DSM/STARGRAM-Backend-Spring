40a702230926938827d4c8dc86d9d35ad3627d62
heesung33 <chaeheesung33@gmail.com>
2022-09-21 12:29:41 +0900

    ➕ :: LikeAlreadyExistsException

---
+++ src/main/java/com/example/stargram/domain/like/exception/LikeAlreadyExistsException.java
@@ -0,0 +1,15 @@
+package com.example.stargram.domain.like.exception;
+
+import com.example.stargram.global.error.exception.ErrorCode;
+import com.example.stargram.global.error.exception.SnsException;
+
+public class LikeAlreadyExistsException extends SnsException {
+
+    public static final SnsException EXCEPTION =
+            new LikeAlreadyExistsException();
+
+    private LikeAlreadyExistsException() {
+        super(ErrorCode.LIKE_ALREADY_EXISTS);
+    }
+
+}
