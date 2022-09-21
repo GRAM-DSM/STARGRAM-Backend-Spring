3948b3fd9cf9b7c5aa58abcc491723d2e76cce06
heesung33 <chaeheesung33@gmail.com>
2022-09-21 11:12:32 +0900

    ➕ :: binary 수정

--- src/main/java/com/example/stargram/domain/feed/domain/Feed.java
+++ src/main/java/com/example/stargram/domain/feed/domain/Feed.java
@@ -20,7 +20,7 @@ public class Feed extends BaseTimeEntity {
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name ="uuid2", strategy = "uuid2")
-    @Column(columnDefinition = "BINARY(16)")
+    @Column(columnDefinition = "BINARY(32)")
     private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY)
