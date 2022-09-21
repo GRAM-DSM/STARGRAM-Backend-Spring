5d1448991845fd0d5349f3d0f055cf84b0f51f9f
heesung33 <chaeheesung33@gmail.com>
2022-09-21 11:13:15 +0900

    ➕ :: BaseTimeEntity

--- src/main/java/com/example/stargram/global/entity/BaseTimeEntity.java
+++ src/main/java/com/example/stargram/global/entity/BaseTimeEntity.java
@@ -1,17 +1,17 @@
 package com.example.stargram.global.entity;
 
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 
 @Getter
 @MappedSuperclass
 public abstract class BaseTimeEntity {
 
-    @CreatedDate
-    private LocalDateTime create_at;
+    @Column(nullable = false)
+    private final ZonedDateTime CreatedAt = ZonedDateTime.now();
 
 }
