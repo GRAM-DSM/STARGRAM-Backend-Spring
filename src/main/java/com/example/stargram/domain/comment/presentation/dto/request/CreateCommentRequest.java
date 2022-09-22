f7f36100ecb90a09bfb4bfe3f3b8a43174857b97
heesung33 <chaeheesung33@gmail.com>
2022-09-22 19:50:01 +0900

    ➕ :: CreateCommnetRequest

--- src/main/java/com/example/stargram/domain/comment/presentation/dto/request/CreateCommentRequest.java
+++ src/main/java/com/example/stargram/domain/comment/presentation/dto/request/CreateCommentRequest.java
@@ -4,9 +4,12 @@ package com.example.stargram.domain.comment.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Getter
 @NoArgsConstructor
 public class CreateCommentRequest {
 
+    private UUID FeedUuid;
     private String content;
 }
