68d1fa6da15e7f99c9dd6c3642da74233c050e34
heesung33 <chaeheesung33@gmail.com>
2022-09-26 20:17:55 +0900

    ➕ :: ImageService

---
+++ src/main/java/com/example/stargram/domain/image/service/ImageService.java
@@ -0,0 +1,31 @@
+package com.example.stargram.domain.image.service;
+
+import com.example.stargram.domain.image.presentation.dto.response.ImageUrlResponse;
+import com.example.stargram.domain.user.domain.User;
+import com.example.stargram.domain.user.facade.UserFacade;
+import com.example.stargram.global.annotation.ServiceWithTransactionalReadOnly;
+import com.example.stargram.global.exception.InvalidRoleException;
+import com.example.stargram.infrastructure.image.s3.S3Facade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@ServiceWithTransactionalReadOnly
+public class ImageService {
+
+    private final S3Facade s3Facade;
+    private final UserFacade userFacade;
+
+    public ImageUrlResponse execute(List<MultipartFile> images) {
+
+        List<String> imageUrl = images.stream()
+                .map(s3Facade::uploadImage)
+                .collect(Collectors.toList());
+
+        return new ImageUrlResponse(imageUrl);
+    }
+
+}
