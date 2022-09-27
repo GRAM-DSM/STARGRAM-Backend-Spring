1040a725efbc6e58c8d780c4d63de8a71391d5b1
heesung33 <chaeheesung33@gmail.com>
2022-09-27 17:31:04 +0900

    🔥 :: S3Util

--- src/main/java/com/example/stargram/infrastructure/image/ImageUtil.java
+++
@@ -1,7 +0,0 @@
-package com.example.stargram.infrastructure.image;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface ImageUtil {
-    String uploadImage(MultipartFile image);
-}
