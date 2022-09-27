dcee058052c0b0ebaac5dba701dc9b1e8a9691ff
heesung33 <chaeheesung33@gmail.com>
2022-09-27 19:37:31 +0900

    ➕ :: ImageService

--- src/main/java/com/example/stargram/domain/image/service/ImageService.java
+++ src/main/java/com/example/stargram/domain/image/service/ImageService.java
@@ -1,9 +1,7 @@
 package com.example.stargram.domain.image.service;
 
-import com.example.stargram.domain.image.domain.Image;
 import com.example.stargram.domain.image.domain.repository.ImageRepository;
 import com.example.stargram.domain.image.presentation.dto.response.ImageUrlResponse;
-import com.example.stargram.domain.user.domain.User;
 import com.example.stargram.domain.user.facade.UserFacade;
 import com.example.stargram.global.annotation.ServiceWithTransactionalReadOnly;
 import com.example.stargram.infrastructure.image.s3.S3Facade;
@@ -22,16 +20,9 @@ public class ImageService {
     private final ImageRepository imageRepository;
 
     public ImageUrlResponse execute(List<MultipartFile> images) {
-        User user = userFacade.getCurrentUser();
-
         List<String> imageUrl = images.stream()
                 .map(s3Facade::uploadImage)
                 .collect(Collectors.toList());
-
-        Image image = Image.builder()
-                .user(user)
-                .build();
-        imageRepository.save(image);
 
         return new ImageUrlResponse(imageUrl);
     }
