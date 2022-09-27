299f30e9eb8313b5cc87a9257e3b90fbb3e7ca98
heesung33 <chaeheesung33@gmail.com>
2022-09-27 17:30:52 +0900

    ➕ :: S3Facade

--- src/main/java/com/example/stargram/infrastructure/image/s3/S3Facade.java
+++ src/main/java/com/example/stargram/infrastructure/image/s3/S3Facade.java
@@ -4,12 +4,9 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.example.stargram.domain.image.domain.Image;
 import com.example.stargram.domain.image.domain.repository.ImageRepository;
-import com.example.stargram.domain.user.domain.User;
 import com.example.stargram.global.exception.ImageValueNotFoundException;
 import com.example.stargram.global.exception.SaveImageFailedException;
-import com.example.stargram.infrastructure.image.ImageUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,16 +15,14 @@ import java.util.UUID;
 
 @RequiredArgsConstructor
 @Component
-public class S3Facade implements ImageUtil {
+public class S3Facade {
 
     private final S3Properties s3Properties;
     private final AmazonS3Client amazonS3Client;
-    private final ImageRepository imageRepository;
 
-    @Override
     public String uploadImage(MultipartFile image) {
 
-        if(image.isEmpty()) {
+        if (image.isEmpty()) {
             throw ImageValueNotFoundException.EXCEPTION;
         }
         String fileName = s3Properties.getBucket() + "/" + UUID.randomUUID() + image.getOriginalFilename();
