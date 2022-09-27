835c308476d49f103d3a2a3880188a45da95f9eb
heesung33 <chaeheesung33@gmail.com>
2022-09-27 19:39:35 +0900

    ➕ :: CreateFeedService

--- src/main/java/com/example/stargram/domain/feed/service/CreateFeedService.java
+++ src/main/java/com/example/stargram/domain/feed/service/CreateFeedService.java
@@ -3,19 +3,23 @@ package com.example.stargram.domain.feed.service;
 import com.example.stargram.domain.feed.domain.Feed;
 import com.example.stargram.domain.feed.domain.repository.FeedRepository;
 import com.example.stargram.domain.feed.presentation.dto.request.CreateFeedRequest;
+import com.example.stargram.domain.image.domain.Image;
+import com.example.stargram.domain.image.domain.repository.ImageRepository;
 import com.example.stargram.domain.user.domain.User;
 import com.example.stargram.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CreateFeedService {
 
     private final FeedRepository feedRepository;
-
     private final UserFacade userFacade;
+    private final ImageRepository imageRepository;
 
+    @Transactional
     public void execute(CreateFeedRequest createFeedRequest) {
         User user = userFacade.getCurrentUser();
 
@@ -26,5 +30,13 @@ public class CreateFeedService {
                 .content(createFeedRequest.getContent())
                 .build();
         feedRepository.save(feed);
+
+        for (String i : createFeedRequest.getUrl()) {
+            Image image = Image.builder()
+                    .feed(feed)
+                    .url(i)
+                    .build();
+            imageRepository.save(image);
+        }
     }
 }
