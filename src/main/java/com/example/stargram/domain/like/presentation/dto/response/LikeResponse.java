65f7d1f4673b68db3d9170196c0bbe2f2d3b02d9
heesung33 <chaeheesung33@gmail.com>
2022-09-28 12:07:23 +0900

    🗄 :: LikeResponse

---
+++ src/main/java/com/example/stargram/domain/like/presentation/dto/response/LikeResponse.java
@@ -0,0 +1,9 @@
+package com.example.stargram.domain.like.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LikeResponse {
+}
