e92afde5280036deec038ce07e63e61efc79405f
heesung33 <chaeheesung33@gmail.com>
2022-09-29 20:41:54 +0900

    ➕ :: LikeController

--- src/main/java/com/example/stargram/domain/like/presentation/LikeController.java
+++ src/main/java/com/example/stargram/domain/like/presentation/LikeController.java
@@ -1,4 +1,31 @@
 package com.example.stargram.domain.like.presentation;
 
+
+import com.example.stargram.domain.like.presentation.dto.response.LikeResponse;
+import com.example.stargram.domain.like.service.AddHeartService;
+import com.example.stargram.domain.like.service.CancelHeartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("likes")
+@RequiredArgsConstructor
+@RestController
 public class LikeController {
+    private final AddHeartService addHeartService;
+    private final CancelHeartService cancelHeartService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{feed-id}")
+    public LikeResponse addHeart(@PathVariable("feed-id") UUID feedId) {
+        return addHeartService.execute(feedId);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{feed-id}")
+    public LikeResponse deleteHeart(@PathVariable("feed-id") UUID feedId) {
+        return cancelHeartService.execute(feedId);
+    }
 }
