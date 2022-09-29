6fb867d3742cb420719357ca208c4a1df12897d4
heesung33 <chaeheesung33@gmail.com>
2022-09-29 20:42:25 +0900

    ➕ :: ErrorCode

--- src/main/java/com/example/stargram/global/error/exception/ErrorCode.java
+++ src/main/java/com/example/stargram/global/error/exception/ErrorCode.java
@@ -11,16 +11,17 @@ public enum ErrorCode {
 
     SAVE_IMAGE_FAILED(400, "COMMON-400-1", "Save Image Failed"),
     IMAGE_VALUE_NOT_FOUND(400, "COMMON-404-1", "Image Value Not Found"),
-    LIKE_ALREADY_EXISTS(409, "LIKE-409-1","Like Already Exists"),
+    LIKE_ALREADY_EXISTS(409, "LIKE-409-1", "Like Already Exists"),
+    LIKE_REMOVE_ALREADY_EXISTS(409, "LIKE-409-1", "Like Remove Already Exists"),
     EXPIRED_JWT(401, "COMMON-401-1", "Expired Jwt"),
     INVALID_JWT(401, "COMMON-401-2", "Invalid Jwt"),
     INTERNAL_SERVER_ERROR(500, "SERVER-500-1", "Internal Server Error"),
-    EXPIRED_ACCESS_TOKEN(401, "AUTH-401-1","Expired Access Token"),
-    EXPIRED_REFRESH_TOKEN(401,"AUTH-401-1","Expired Refresh Token"),
+    EXPIRED_ACCESS_TOKEN(401, "AUTH-401-1", "Expired Access Token"),
+    EXPIRED_REFRESH_TOKEN(401, "AUTH-401-1", "Expired Refresh Token"),
     INVALID_ROLE(401, "GLOBAL-401-1", "Invalid Role"),
-    INVALID_TOKEN(401,"AUTH-401-1","Invalid Token"),
-    INCORRECT_TOKEN(500,"AUTH-500-1","Incrrect Token"),
-    INVALID_PASSWORD(401,"USER-4o1-1", "Invalid Passowrd"),
+    INVALID_TOKEN(401, "AUTH-401-1", "Invalid Token"),
+    INCORRECT_TOKEN(500, "AUTH-500-1", "Incrrect Token"),
+    INVALID_PASSWORD(401, "USER-4o1-1", "Invalid Passowrd"),
     USER_NOT_FOUND(404, "USER-404-1", "User Not Found"),
     FEED_NOT_FOUND(404, "FEED-404-1", "Feed Not Found");
     private final int status;
