0021b7532f84b01119dd39f824d692c1dcc98a66
heesung33 <chaeheesung33@gmail.com>
2022-09-29 20:41:13 +0900

    ➕ :: LikeRepository

--- src/main/java/com/example/stargram/domain/like/domain/repository/LikeRepository.java
+++ src/main/java/com/example/stargram/domain/like/domain/repository/LikeRepository.java
@@ -3,8 +3,11 @@ package com.example.stargram.domain.like.domain.repository;
 import com.example.stargram.domain.feed.domain.Feed;
 import com.example.stargram.domain.like.domain.Like;
 import com.example.stargram.domain.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface LikeRepository extends JpaRepository<Like, Long> {
-    boolean existsById(User user, Feed feed);
+public interface LikeRepository extends CrudRepository<Like, Long> {
+
+    boolean existsByUserAndFeed(User user, Feed feed);
+
+    void deleteByUserAndFeed(User user, Feed feed);
 }
