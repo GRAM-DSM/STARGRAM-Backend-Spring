6198376bb5993077f3d863126512ce6de6d1aafa
heesung33 <chaeheesung33@gmail.com>
2022-10-04 10:35:36 +0900

    ➕ :: BookMark

---
+++ src/main/java/com/example/stargram/domain/bookmark/domain/BookMark.java
@@ -0,0 +1,37 @@
+package com.example.stargram.domain.bookmark.domain;
+
+import com.example.stargram.domain.feed.domain.Feed;
+import com.example.stargram.domain.user.domain.User;
+import com.sun.istack.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "BookmarkTbl")
+@Entity
+public class BookMark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
+    private Feed feed;
+
+    @Builder
+    public BookMark(User user, Feed feed) {
+        this.feed = feed;
+        this.user = user;
+    }
+}
