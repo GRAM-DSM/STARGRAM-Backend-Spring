8bcc25b092998776efd74becdb0eee241dd2796f
heesung33 <chaeheesung33@gmail.com>
2022-10-04 17:18:20 +0900

    🔥 :: JoinFetch

--- src/main/java/com/example/stargram/domain/feed/domain/repository/FeedRepository.java
+++ src/main/java/com/example/stargram/domain/feed/domain/repository/FeedRepository.java
@@ -1,14 +1,10 @@
 package com.example.stargram.domain.feed.domain.repository;
 
 import com.example.stargram.domain.feed.domain.Feed;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface FeedRepository extends CrudRepository<Feed, UUID> {
 
-    @Query
-    List<Feed> findAllByJoinFetch();
 }
