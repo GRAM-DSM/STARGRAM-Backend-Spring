1baee256df3c3d78677d61224beef2b4b71cd72f
heesung33 <chaeheesung33@gmail.com>
2022-10-04 17:18:46 +0900

    ➕ :: BookMarkRepository

--- src/main/java/com/example/stargram/domain/bookmark/domain/repository/BookMarkRepository.java
+++ src/main/java/com/example/stargram/domain/bookmark/domain/repository/BookMarkRepository.java
@@ -3,5 +3,5 @@ package com.example.stargram.domain.bookmark.domain.repository;
 import com.example.stargram.domain.bookmark.domain.BookMark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookMarkRepository extends JpaRepository<Long, BookMark> {
+public interface BookMarkRepository extends JpaRepository<BookMark, Long> {
 }
