5c7df3ee63ea7735574cc1911394cdad9f8bb151
heesung33 <chaeheesung33@gmail.com>
2022-10-12 10:49:36 +0900

    ➕ :: ProfileRepository

---
+++ src/main/java/com/example/stargram/domain/profile/domain/repository/ProfileRepository.java
@@ -0,0 +1,7 @@
+package com.example.stargram.domain.profile.domain.repository;
+
+import com.example.stargram.domain.profile.domain.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfileRepository extends JpaRepository<Profile, String> {
+}
