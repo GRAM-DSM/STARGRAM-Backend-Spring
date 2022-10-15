bfed86f56c035d63f0635997bafcf39f08b492ec
heesung33 <chaeheesung33@gmail.com>
2022-10-15 23:26:29 +0900

    FeedGetService

--- src/main/java/com/example/stargram/domain/feed/service/FeedGetService.java
+++ src/main/java/com/example/stargram/domain/feed/service/FeedGetService.java
@@ -1,4 +1,13 @@
 package com.example.stargram.domain.feed.service;
 
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@NoArgsConstructor
+@Service
 public class FeedGetService {
+
+
+
 }
