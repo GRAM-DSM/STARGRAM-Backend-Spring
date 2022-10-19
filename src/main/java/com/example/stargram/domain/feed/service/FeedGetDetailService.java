bc528eda1ad733f6ccc88e61dbd8720b566008b0
heesung33 <chaeheesung33@gmail.com>
2022-10-19 23:27:17 +0900

    FeedGetDetailService

--- src/main/java/com/example/stargram/domain/feed/service/FeedGetDetailService.java
+++ src/main/java/com/example/stargram/domain/feed/service/FeedGetDetailService.java
@@ -2,9 +2,12 @@ package com.example.stargram.domain.feed.service;
 
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @NoArgsConstructor
 @Service
 public class FeedGetDetailService {
 
+    @Transactional
+    public
 }
