5b8e4d3539de94ca5bdd444e4e0dd8cb15245467
heesung33 <chaeheesung33@gmail.com>
2022-10-25 19:53:50 +0900

    ➕ :: FeedGetResponse

--- src/main/java/com/example/stargram/domain/feed/presentation/dto/response/FeedGetResponse.java
+++ src/main/java/com/example/stargram/domain/feed/presentation/dto/response/FeedGetResponse.java
@@ -1,4 +1,32 @@
 package com.example.stargram.domain.feed.presentation.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+@AllArgsConstructor
+@Getter
 public class FeedGetResponse {
+
+    private final List<FeedResponse> feedResponseList;
+
+    @Getter
+    @Builder
+    public static class FeedResponse {
+
+        UUID feedUuid;
+        String name;
+        String title;
+        ZonedDateTime createAt;
+        int heartCount;
+        int commentCount;
+        String image;
+        boolean heartType;
+    }
+
 }
