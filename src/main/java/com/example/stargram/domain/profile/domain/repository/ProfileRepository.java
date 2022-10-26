44e36e720af2b162f11a0add634ba21796969d09
heesung33 <chaeheesung33@gmail.com>
2022-10-26 09:22:05 +0900

    ➕ :: ProfileRepository

--- src/main/java/com/example/stargram/domain/profile/domain/repository/ProfileRepository.java
+++ src/main/java/com/example/stargram/domain/profile/domain/repository/ProfileRepository.java
@@ -4,5 +4,9 @@ package com.example.stargram.domain.profile.domain.repository;
 import com.example.stargram.domain.profile.domain.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ProfileRepository extends JpaRepository<Profile, String> {
+    Optional<Profile> findByUserId(UUID userId);
 }
