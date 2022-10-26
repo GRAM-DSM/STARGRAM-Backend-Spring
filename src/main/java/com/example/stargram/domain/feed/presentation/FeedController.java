0b3621353b9db9bb93bc0c21ca8d8c411bdaf07c
heesung33 <chaeheesung33@gmail.com>
2022-10-26 09:21:06 +0900

    ➕ :: FeedController

--- src/main/java/com/example/stargram/domain/feed/presentation/FeedController.java
+++ src/main/java/com/example/stargram/domain/feed/presentation/FeedController.java
@@ -2,8 +2,10 @@ package com.example.stargram.domain.feed.presentation;
 
 import com.example.stargram.domain.feed.presentation.dto.request.CreateFeedRequest;
 import com.example.stargram.domain.feed.presentation.dto.request.UpdateFeedRequest;
+import com.example.stargram.domain.feed.presentation.dto.response.FeedGetResponse;
 import com.example.stargram.domain.feed.service.CreateFeedService;
 import com.example.stargram.domain.feed.service.DeleteFeedService;
+import com.example.stargram.domain.feed.service.FeedGetService;
 import com.example.stargram.domain.feed.service.UpdateFeedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,6 +21,12 @@ public class FeedController {
     private final CreateFeedService createFeedService;
     private final DeleteFeedService deleteFeedService;
     private final UpdateFeedService updateFeedService;
+    private final FeedGetService feedGetService;
+
+    @GetMapping("/list")
+    public FeedGetResponse getFeed() {
+        return feedGetService.execute();
+    }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
@@ -26,6 +34,7 @@ public class FeedController {
         createFeedService.execute(request);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteFeed(@PathVariable UUID id) {
         deleteFeedService.execute(id);
@@ -36,4 +45,5 @@ public class FeedController {
     public void updateFeed(@PathVariable UUID id, @RequestBody UpdateFeedRequest request) {
         updateFeedService.execute(id, request);
     }
+
 }
