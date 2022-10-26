ace17a83866dc591b03e4d8304cd2a8ba0a6353e
heesung33 <chaeheesung33@gmail.com>
2022-10-26 09:21:12 +0900

    ➕ :: FeedGetService

--- src/main/java/com/example/stargram/domain/feed/service/FeedGetService.java
+++ src/main/java/com/example/stargram/domain/feed/service/FeedGetService.java
@@ -5,8 +5,12 @@ import com.example.stargram.domain.feed.domain.Feed;
 import com.example.stargram.domain.feed.domain.repository.FeedRepository;
 import com.example.stargram.domain.feed.presentation.dto.response.FeedGetResponse;
 import com.example.stargram.domain.image.domain.Image;
+import com.example.stargram.domain.image.domain.repository.ImageRepository;
+import com.example.stargram.domain.image.exception.ImageNotFoundException;
 import com.example.stargram.domain.like.facade.LikeFacade;
 import com.example.stargram.domain.profile.domain.Profile;
+import com.example.stargram.domain.profile.domain.repository.ProfileRepository;
+import com.example.stargram.domain.profile.exception.ProfileNotFoundException;
 import com.example.stargram.domain.user.domain.User;
 import com.example.stargram.domain.user.facade.UserFacade;
 import com.example.stargram.global.entity.BaseTimeEntity;
@@ -15,6 +19,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -24,28 +29,31 @@ public class FeedGetService {
     private final LikeFacade likeFacade;
     private final UserFacade userFacade;
     private final FeedRepository feedRepository;
+    private final ProfileRepository profileRepository;
+    private final ImageRepository imageRepository;
 
-    @Transactional
+    @Transactional(readOnly = true)
     public FeedGetResponse execute() {
         User user = userFacade.getCurrentUser();
+        Profile profile = profileRepository.findByUserId(user.getId()).orElseThrow(() -> ProfileNotFoundException.EXCEPTION);
         List<FeedGetResponse.FeedResponse> feedResponseList = feedRepository.findAllByJoinFetch()
                 .stream()
-                .map(this::feedList)
+                .map(feed -> {
+                    FeedGetResponse.FeedResponse feedResponse = FeedGetResponse.FeedResponse.builder()
+                            .feedUuid(feed.getId())
+                            .title(feed.getTitle())
+                            .heartCount(feed.getHeartCount())
+                            .commentCount(feed.getCommentCount())
+                            .createAt(feed.getCreateAt())
+                            .name(profile.getName())
+                            .image(imageRepository.findByFeedId(feed.getId()).orElseThrow(()-> ImageNotFoundException.EXCEPTION).getUrl())
+                            .heartType(likeFacade.confirmHeart(user,feed))
+                            .build();
+                    return feedResponse;
+                })
                 .collect(Collectors.toList());
         return new FeedGetResponse(feedResponseList);
-    }
 
-    private FeedGetResponse.FeedResponse feedList(Feed feed, User user, BaseTimeEntity baseTimeEntity, Profile profile, Image image) {
-        return FeedGetResponse.FeedResponse.builder()
-                .feedUuid(feed.getId())
-                .title(feed.getTitle())
-                .heartCount(feed.getHeartCount())
-                .commentCount(feed.getCommentCount())
-                .createAt(baseTimeEntity.getCreateAt())
-                .name(profile.getName())
-                .image(image.getUrl())
-                .heartType(likeFacade.confirmHeart(user, feed))
-                .build();
     }
 
 }
