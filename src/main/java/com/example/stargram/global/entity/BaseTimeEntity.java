aab97fe62a1c7681c4a3a1fdc96c42d27dfea8b3
heesung33 <chaeheesung33@gmail.com>
2022-10-26 09:21:46 +0900

    ➕ :: BaseTimeEntity

--- src/main/java/com/example/stargram/global/entity/BaseTimeEntity.java
+++ src/main/java/com/example/stargram/global/entity/BaseTimeEntity.java
@@ -1,10 +1,13 @@
 package com.example.stargram.global.entity;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import java.time.ZonedDateTime;
+
 
 
 @Getter
