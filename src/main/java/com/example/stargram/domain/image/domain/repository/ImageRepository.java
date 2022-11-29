95fb5add250688e15668c409a7162836d61bc7e5
heesung33 <chaeheesung33@gmail.com>
2022-11-29 11:16:44 +0900

    ➕ :: ImageRepository

--- src/main/java/com/example/stargram/domain/image/domain/repository/ImageRepository.java
+++ src/main/java/com/example/stargram/domain/image/domain/repository/ImageRepository.java
@@ -4,9 +4,8 @@ import com.example.stargram.domain.image.domain.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface ImageRepository extends JpaRepository<Image, UUID> {
-    List<Image> findAllByFeed_Id(UUID feedId);
+    List<Image> findAllByFeedId(UUID feedId);
 }
