d81c478c989b125fe1568591a840fc03c91e38c5
heesung33 <chaeheesung33@gmail.com>
2022-11-29 11:16:27 +0900

    ➕ :: FeedGetDetailService

--- src/main/java/com/example/stargram/domain/feed/service/FeedGetDetailService.java
+++ src/main/java/com/example/stargram/domain/feed/service/FeedGetDetailService.java
@@ -1,19 +1,67 @@
 package com.example.stargram.domain.feed.service;
 
-/*
 
-import com.example.stargram.domain.feed.presentation.dto.response.FeedGetResponse;
-import lombok.NoArgsConstructor;
+
+import com.example.stargram.domain.comment.domain.repository.CommentsRepository;
+import com.example.stargram.domain.feed.domain.repository.FeedRepository;
+import com.example.stargram.domain.feed.exception.FeedNotFoundException;
+import com.example.stargram.domain.feed.presentation.dto.response.FeedDetailResponse;
+import com.example.stargram.domain.image.domain.Image;
+import com.example.stargram.domain.image.domain.repository.ImageRepository;
+import com.example.stargram.domain.like.facade.LikeFacade;
+import com.example.stargram.domain.profile.domain.Profile;
+import com.example.stargram.domain.profile.domain.repository.ProfileRepository;
+import com.example.stargram.domain.profile.exception.ProfileNotFoundException;
+import com.example.stargram.domain.user.domain.User;
+import com.example.stargram.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@NoArgsConstructor
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
 @Service
 public class FeedGetDetailService {
 
-    @Transactional
-    public FeedGetResponse execute() {
+    private final LikeFacade likeFacade;
+    private final UserFacade userFacade;
+    private final FeedRepository feedRepository;
+    private final ProfileRepository profileRepository;
+    private final ImageRepository imageRepository;
+    private final CommentsRepository commentsRepository;
+
+    @Transactional(readOnly = true)
+    public FeedDetailResponse.FeedGetDetailResponse execute(UUID id) {
+        User user = userFacade.getCurrentUser();
+        Profile profile = profileRepository.findByUserId(user.getId()).orElseThrow(() -> ProfileNotFoundException.EXCEPTION);
+        return feedRepository.findById(id)
+                .map(feed -> {
+                    FeedDetailResponse.FeedGetDetailResponse feedGetDetailResponse = FeedDetailResponse.FeedGetDetailResponse.builder()
+                            .feedUuid(feed.getId())
+                            .profileImage(profile.getPhoto())
+                            .name(profile.getName())
+                            .title(feed.getTitle())
+                            .content(feed.getContent())
+                            .createAt(feed.getCreateAt())
+                            .category(feed.getCategory())
+                            .heartCount(feed.getHeartCount())
+                            .commentCount(feed.getCommentCount())
+                            .favorite(likeFacade.confirmHeart(user, feed))
+                            .images(imageRepository.findAllByFeedId(feed.getId()).stream().map(Image::getUrl).collect(Collectors.toList()))
+                            .comments(commentsRepository.findAllByFeedId(feed.getId()).stream().map(comments -> FeedDetailResponse.CommentsResponse.builder()
+                                    .commentId(comments.getId())
+                                    .profileImage(profileRepository.findByUserId(feed.getUser().getId()).orElseThrow(() -> ProfileNotFoundException.EXCEPTION).getPhoto())
+                                    .content(comments.getContent())
+                                    .name(profile.getName())
+                                    .build()).collect(Collectors.toList()))
+                            .build();
+                    return feedGetDetailResponse;
+                })
+                .orElseThrow(() -> FeedNotFoundException.EXCEPTION);
 
     }
 }
-*/
+
+
