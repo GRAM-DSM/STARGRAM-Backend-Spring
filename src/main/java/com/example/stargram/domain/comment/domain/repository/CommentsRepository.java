23df1d6785530bef731cd18020603952eb24f295
heesung33 <chaeheesung33@gmail.com>
2022-11-29 11:14:49 +0900

    ➕ :: CommentsRepository

--- src/main/java/com/example/stargram/domain/comment/domain/repository/CommentsRepository.java
+++ src/main/java/com/example/stargram/domain/comment/domain/repository/CommentsRepository.java
@@ -3,7 +3,12 @@ package com.example.stargram.domain.comment.domain.repository;
 import com.example.stargram.domain.comment.domain.Comments;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 
 public interface CommentsRepository extends CrudRepository<Comments, Long> {
 
+    List<Comments> findAllByFeedId(UUID id);
 }
