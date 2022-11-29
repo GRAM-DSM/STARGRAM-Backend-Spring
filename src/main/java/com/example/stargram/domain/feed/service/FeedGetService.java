1365fdee5e4321dfd5e56debe45e884e35553229
heesung33 <chaeheesung33@gmail.com>
2022-11-29 11:16:34 +0900

    ➕ :: FeedGetService

--- src/main/java/com/example/stargram/domain/feed/service/FeedGetService.java
+++ src/main/java/com/example/stargram/domain/feed/service/FeedGetService.java
@@ -1,25 +1,21 @@
 package com.example.stargram.domain.feed.service;
 
 
-import com.example.stargram.domain.feed.domain.Feed;
 import com.example.stargram.domain.feed.domain.repository.FeedRepository;
 import com.example.stargram.domain.feed.presentation.dto.response.FeedGetResponse;
 import com.example.stargram.domain.image.domain.Image;
 import com.example.stargram.domain.image.domain.repository.ImageRepository;
-import com.example.stargram.domain.image.exception.ImageNotFoundException;
 import com.example.stargram.domain.like.facade.LikeFacade;
 import com.example.stargram.domain.profile.domain.Profile;
 import com.example.stargram.domain.profile.domain.repository.ProfileRepository;
 import com.example.stargram.domain.profile.exception.ProfileNotFoundException;
 import com.example.stargram.domain.user.domain.User;
 import com.example.stargram.domain.user.facade.UserFacade;
-import com.example.stargram.global.entity.BaseTimeEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -46,7 +42,7 @@ public class FeedGetService {
                             .commentCount(feed.getCommentCount())
                             .createAt(feed.getCreateAt())
                             .name(profile.getName())
-                            .image(imageRepository.findAllByFeed_Id(feed.getId()).stream().map(Image::getUrl).collect(Collectors.toList()))
+                            .image(imageRepository.findAllByFeedId(feed.getId()).stream().map(Image::getUrl).collect(Collectors.toList()))
                             .heartType(likeFacade.confirmHeart(user,feed))
                             .build();
                     return feedResponse;
