4680d2ac542e84959b4914cd1ee8f13fcffd2dc7
heesung33 <chaeheesung33@gmail.com>
2022-12-21 22:54:13 +0900

    url 수정

--- src/main/java/com/example/stargram/domain/like/presentation/LikeController.java
+++ src/main/java/com/example/stargram/domain/like/presentation/LikeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@RequestMapping("likes")
+@RequestMapping("feeds/likes")
 @RequiredArgsConstructor
 @RestController
 public class LikeController {
